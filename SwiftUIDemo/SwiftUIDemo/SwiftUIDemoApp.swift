ee81ba5bd0f8e880330e1d03bce714be83af5e8e
Vidhi Patel <vidhi.patel@reversebits.tech>
2024-02-26 10:49:05 +0530

    Initial Project.

---
+++ SwiftUIDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Vidhi Patel reverseBits on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
