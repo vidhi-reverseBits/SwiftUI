fbf2e5220d1e8f7ac0b017ced7b6f4d7473d791b
Vidhi Patel <vidhi.patel@reversebits.tech>
2024-03-09 17:27:47 +0530

    Toggle demo

--- SwiftUIDemo/SwiftUIDemo/UIComponents/TextFieldDemo.swift
+++ SwiftUIDemo/SwiftUIDemo/UIComponents/TextFieldDemo.swift
@@ -11,19 +11,29 @@ struct TextFieldDemo: View {
     @State private var username: String = ""
     @State private var password: String = ""
     @State private var age = 0
+    @State private var bio = "About me"
     
     var body: some View {
+        VStack {
+            TextField("Describe yourself", text: $bio, axis: .vertical)
+                .textFieldStyle(.roundedBorder)
+                .lineLimit(5)
+        }
+        .padding()
+        
         Form {
-            TextField("Username", text: $username)
-                .submitLabel(.next)
-                .textFieldStyle(.roundedBorder)
-            SecureField("Password", text: $password)
-                .submitLabel(.next)
-                .textFieldStyle(.roundedBorder)
-            TextField("Age", value: $age, format: .number)
-                .submitLabel(.done)
-                .textFieldStyle(.roundedBorder)
-                .keyboardType(.numberPad)
+            Section(header: Text("Information")) {
+                TextField("Username", text: $username)
+                    .submitLabel(.next)
+                    .textFieldStyle(.roundedBorder)
+                SecureField("Password", text: $password)
+                    .submitLabel(.next)
+                    .textFieldStyle(.roundedBorder)
+                TextField("Age", value: $age, format: .number)
+                    .submitLabel(.done)
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.numberPad)
+            }
         }
         .onSubmit {
             guard username.isEmpty == false && password.isEmpty == false else { return }
---
+++ SwiftUIDemo/SwiftUIDemo/UIComponents/ToggleDemo.swift
@@ -0,0 +1,55 @@
+//
+//  ToggleDemo.swift
+//  SwiftUIDemo
+//
+//  Created by Vidhi Patel reverseBits on 09/03/24.
+//
+
+import SwiftUI
+
+struct ToggleDemo: View {
+    @State private var showGreeting: Bool = true
+    @State private var isOn: Bool = false
+    @State var lists = [
+            EmailList(id: "Monthly Updates", isSubscribed: true),
+            EmailList(id: "News Flashes", isSubscribed: true),
+            EmailList(id: "Special Offers", isSubscribed: true)
+        ]
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 20) {
+            Toggle("Show welcome message", isOn: $showGreeting)
+                .padding()
+                .toggleStyle(SwitchToggleStyle(tint: .red))
+            if(showGreeting) {
+                Text("Hello World!")
+            }
+        }
+        
+        Toggle("Filter", isOn: $isOn)
+            .toggleStyle(.button)
+            .tint(.mint)
+            .padding()
+        
+        Form {
+            Section {
+                ForEach($lists) { $list in
+                    Toggle(list.id, isOn: $list.isSubscribed)
+                }
+            }
+            
+            Section {
+                Toggle("Subscribe to all", sources: $lists, isOn: \.isSubscribed)
+            }
+        }
+    }
+}
+
+#Preview {
+    ToggleDemo()
+}
+
+struct EmailList: Identifiable {
+    var id: String
+    var isSubscribed = false
+}
